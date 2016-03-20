2f9c40073edaa0005fb5a374ac1df37303af07a4
Toni Michel <toni.michel@schnapptack.de>
2016-03-20 12:55:17 +0100

    added ape to sys path in order to run tests directly

--- ape/test/run.py
+++ ape/test/run.py
@@ -1,16 +1,11 @@
 import sys
 import os
+APE_PTH = os.path.abspath(os.path.dirname(__file__))
 # add the installation script dir to the path.
-sys.path.append(
-    os.path.abspath(
-        os.path.join(
-            os.path.abspath(
-                os.path.dirname(__file__)
-            ),
-            '../../bin/'
-        )
-    )
-)
+sys.path.append(os.path.join(APE_PTH,  '../../bin/'))
+# add the ape dir to the path in order to allow importing ape
+# which is required to running tests.
+sys.path.append(os.path.join(APE_PTH,  '../../'))
 from ape import test
 
 
@@ -18,8 +13,6 @@ from ape import test
 #   Further test cases are placed within ape.test.
 #   To add new test cases add them in their own module within
 #   ape/test and register them in ape/test/__init__.py
-
-
 
 if __name__ == '__main__':
 
