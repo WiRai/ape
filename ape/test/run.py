d5417a3adc9f46167e9b9090cfe52c5c55d5fc5e
Hendrik Speidel <hendrik@schnapptack.de>
2016-03-25 21:32:02 +0100

    refactor test script

--- ape/test/run.py
+++ ape/test/run.py
@@ -1,21 +1,23 @@
 import sys
 import os
+
 APE_PTH = os.path.abspath(os.path.dirname(__file__))
-# add the installation script dir to the path.
-sys.path.append(os.path.join(APE_PTH,  '../../bin/'))
-# add the ape dir to the path in order to allow importing ape
-# which is required to running tests.
-sys.path.append(os.path.join(APE_PTH,  '../../'))
-from ape import test
 
+def run():
+    '''This is the main test runner of ape.
 
-#   This is the main test runner of ape.
-#   Further test cases are placed within ape.test.
-#   To add new test cases add them in their own module within
-#   ape/test and register them in ape/test/__init__.py
+    Further test cases are placed within ape.test.
+    To add new test cases add them in their own module within
+    ape/test and register them in ape/test/__init__.py
+    '''
+    from ape import test
+    result = test.run_all()
+    return 0 if result.wasSuccessful() else 1
+
 
 if __name__ == '__main__':
 
-    result = test.run_all()
-    retval = 0 if result.wasSuccessful() else 1
-    sys.exit(retval)
+    # add the ape dir to the path in order to allow importing ape
+    # which is required to running tests.
+    sys.path.append(os.path.join(APE_PTH,  '../../'))
+    sys.exit(run())
