b55771f164eadb59a392d5e73d088a844ab35ef3
Hendrik Speidel <hendrik@schnapptack.de>
2016-03-26 23:36:14 +0100

    add path hack to run tests via selftest task

--- ape/test/installation/__init__.py
+++ ape/test/installation/__init__.py
@@ -1,2 +1,10 @@
+
+# path hack to place ape_install on the python path, so we can import it for testing
+import sys
+from os.path import abspath, dirname, join
+APE_PTH = abspath(dirname(__file__))
+sys.path.append(join(APE_PTH,  '../../../bin/'))
+
+# import your tests here
 from . install_tests import *
 from . commandlineparser_tests import *
